a10100cb3b9210bd2fd54b3627187fa3aef591b8
Stephen Fox <stephen@capablecreative.com>
2019-09-14 18:56:54 -0500

    All the framework is in place for the various API calls. There is a LOT of redundant code that could be fixed with THIS instead of specific classes and queries. Feel free to clean up, or I can in V1.5 - SLF

--- assets/js/tgif.js
+++ assets/js/tgif.js
@@ -12,6 +12,31 @@ $(".delCatInputBtn").submit(function(e) {
 $(".recCatInputBtn").submit(function(e) {
     e.preventDefault();
 });
+$(".movCatInputBtn").submit(function(e) {
+    e.preventDefault();
+});
+$(".musCatInputBtn").submit(function(e) {
+    e.preventDefault();
+});
+$(".booCatInputBtn").submit(function(e) {
+    e.preventDefault();
+});
+
+/* =====================================================
+-------------- PATH SELECTION --------------------------
+===================================================== */
+$(".todo-select").on("click",function(){
+    $(".todo-select").removeClass("inactive");
+    $(".dinner-select").addClass("inactive");
+    $(".todo-selection").css("display","block");
+    $(".dinner-selection").css("display","none");
+});
+$(".dinner-select").on("click",function(){
+    $(".dinner-select").removeClass("inactive");
+    $(".todo-select").addClass("inactive");
+    $(".todo-selection").css("display","none");
+    $(".dinner-selection").css("display","block");
+});
 
 /* =====================================================
 -------------- DELIVERY PATH ---------------------------
@@ -22,6 +47,10 @@ $("#deliverIt").on("click", function(){
     $("#enterZip").css("display","block");
     $("#enterZip").animate({opacity: 1.0});
     $(".zipVarStore").css("display","block");
+});
+$(".zipVarStore").on("click",function(){
+    $("#enterZip").css("display","none");
+    $("#deliverIt").css("display","block");
 });
 // -- User's entered zipcode is stored in the 'userZip' variable. Clicking also displays Delivery Categories
 $(".zipVarStore").on("click",function(){
@@ -66,7 +95,73 @@ $("#getRecipe").on("click",function(){
     });    
 });
 
-// -- Close various modals with "x" span - SLF
+/* =====================================================
+-------------- MOVIE PATH ------------------------------
+===================================================== */
+// -- On Choosing "WATCH A MOVIE." on main page, music categories display.
+$("#watchMovie").on("click",function(){   
+    $(".movieOptionsWrap").css("display","block"); 
+    $(".movieOptionsWrap").animate({opacity: 1.0});
+    $(".movieCat.btn").on("click",function(){
+        let movieCategory = $(this).attr("data-value");
+        $(".row.movie-options").css("display","none");
+        $(".movie-api.row").animate({opacity:1.0});
+        movieVarStore(movieCategory);
+    });
+// -- IF user enters specific search criteria, that data is stored in the 'movieCategory' variable     
+    $(".movCatInputBtn").on("click",function(){
+        let movieCategory = $("#movCatInput").val().trim().toLowerCase(); 
+        $(".row.movie-options").css("display","none");
+        $(".movie-api.row").animate({opacity:1.0});
+        movieVarStore(movieCategory);
+    });    
+});
+
+/* =====================================================
+-------------- MUSIC PATH ---------------------------
+===================================================== */
+// -- On Choosing "LISTEN TO MUSIC." on main page, music categories display.
+$("#listenMusic").on("click",function(){   
+    $(".musicOptionsWrap").css("display","block"); 
+    $(".musicOptionsWrap").animate({opacity: 1.0});
+    $(".musicCat.btn").on("click",function(){
+        let musicCategory = $(this).attr("data-value");
+        $(".row.music-options").css("display","none");
+        $(".music-api.row").animate({opacity:1.0});
+        musicVarStore(musicCategory);
+    });
+// -- IF user enters specific search criteria, that data is stored in the 'musicCategory' variable     
+    $(".musCatInputBtn").on("click",function(){
+        let musicCategory = $("#musCatInput").val().trim().toLowerCase(); 
+        $(".row.music-options").css("display","none");
+        $(".music-api.row").animate({opacity:1.0});
+        musicVarStore(musicCategory);
+    });    
+});
+
+/* =====================================================
+-------------- BOOK PATH -------------------------------
+===================================================== */
+// -- On Choosing "READ A BOOK." on main page, book categories display.
+$("#readBook").on("click",function(){   
+    $(".bookOptionsWrap").css("display","block"); 
+    $(".bookOptionsWrap").animate({opacity: 1.0});
+    $(".bookCat.btn").on("click",function(){
+        let bookCategory = $(this).attr("data-value");
+        $(".row.book-options").css("display","none");
+        $(".book-api.row").animate({opacity:1.0});
+        bookVarStore(bookCategory);
+    });
+// -- IF user enters specific search criteria, that data is stored in the 'bookCategory' variable     
+    $(".booCatInputBtn").on("click",function(){
+        let bookCategory = $("#booCatInput").val().trim().toLowerCase(); 
+        $(".row.book-options").css("display","none");
+        $(".book-api.row").animate({opacity:1.0});
+        bookVarStore(bookCategory);
+    });    
+});
+
+// -- Close various modals with "x" span. These two are for DELIVERY - SLF
 $(".closeModal").on("click",function(){
     $(".optionsModalDel1").css("display","none");
     $(".optionsModalDel1").animate({opacity:0.0});
@@ -74,7 +169,9 @@ $(".closeModal").on("click",function(){
 $(".closeModal2").on("click",function(){
     $(".deliverOptionsWrap").css("display","none");
     $(".deliverOptionsWrap").animate({opacity:0.0});
+    location.reload();
 });
+// -- Close modals for RECIPES - SLF
 $(".closeModal3").on("click",function(){
     $(".optionsModalRec2").css("display","none");
     $(".optionsModalRec2").animate({opacity:0.0});
@@ -82,7 +179,39 @@ $(".closeModal3").on("click",function(){
 $(".closeModal4").on("click",function(){
     $(".recipeOptionsWrap").css("display","none");
     $(".recipeOptionsWrap").animate({opacity:0.0});
+    location.reload();
 });
+// -- Close modals for MOVIES - SLF
+$(".closeModal5").on("click",function(){
+    $(".optionsModalMov3").css("display","none");
+    $(".optionsModalMov3").animate({opacity:0.0});
+});
+$(".closeModal6").on("click",function(){
+    $(".movieOptionsWrap").css("display","none");
+    $(".movieOptionsWrap").animate({opacity:0.0});
+    location.reload();
+});
+// -- Close modals for MUSIC - SLF
+$(".closeModal7").on("click",function(){
+    $(".optionsModalMus4").css("display","none");
+    $(".optionsModalMus4").animate({opacity:0.0});
+});
+$(".closeModal8").on("click",function(){
+    $(".musciOptionsWrap").css("display","none");
+    $(".musicOptionsWrap").animate({opacity:0.0});
+    location.reload();
+});
+// -- Close modals for BOOKS - SLF
+$(".closeModal9").on("click",function(){
+    $(".optionsModalBoo5").css("display","none");
+    $(".optionsModalBoo5").animate({opacity:0.0});
+    location.reload();
+});
+$(".closeModal10").on("click",function(){
+    $(".bookOptionsWrap").css("display","none");
+    $(".bookOptionsWrap").animate({opacity:0.0});
+});
+
 
 // -- This function holds variables created through user selections on the DELIVERY PATH, and passes them as arguments here - SLF
 function varStore(zipCode,delCategory){
@@ -92,4 +221,16 @@ function varStore(zipCode,delCategory){
 // -- This function holds variables created through user selections on the RECIPE PATH, and passes them as arguments here - SLF
 function recipeVarStore(userRecipeCategory){
     console.log("Selected Recipe Category is: " + userRecipeCategory);
+}
+// -- This function holds variables created through user selections on the MOVIE PATH, and passes them as arguments here - SLF
+function movieVarStore(userMovieCategory){
+    console.log("Selected Movie Category is: " + userMovieCategory);
+}
+// -- This function holds variables created through user selections on the MUSIC PATH, and passes them as arguments here - SLF
+function musicVarStore(userMusicCategory){
+    console.log("Selected Music Category is: " + userMusicCategory);
+}
+// -- This function holds variables created through user selections on the BOOK PATH, and passes them as arguments here - SLF
+function bookVarStore(userBookCategory){
+    console.log("Selected Book Category is: " + userBookCategory);
 }
